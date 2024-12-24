1b2ce55cbe4e719aa3904467cb39010eb4beccf3
opfromthestart <opfromthestart@gmail.com>
2024-12-23 19:58:44 -0500

    add disconnect function

--- netstack/src/client/mod.rs
+++ netstack/src/client/mod.rs
@@ -1,9 +1,9 @@
-use failure::Error;
-use crate::connection::*;
 use super::transport::Transport;
-use std::net::SocketAddr;
-use std::collections::HashMap;
+use crate::connection::*;
 use crate::packets::RawPacket;
+use failure::Error;
+use std::collections::HashMap;
+use std::net::SocketAddr;
 
 mod configuration;
 pub use configuration::Configuration;
@@ -14,9 +14,9 @@ pub use event::Event;
 mod error;
 pub use error::ClientError;
 
-use crate::security::{Secret, ConnectionToken, ReplayBuffer};
-use crate::packets::{OutgoingPacket, PacketType};
 use crate::monitoring::ClientMonitor;
+use crate::packets::{OutgoingPacket, PacketType};
+use crate::security::{ConnectionToken, ReplayBuffer, Secret};
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum ConnectionState {
@@ -43,7 +43,11 @@ pub struct Client {
 }
 
 impl Client {
-    pub fn new(configuration: Configuration, transport: Box<dyn Transport>, monitor: Box<dyn ClientMonitor>) -> Self {
+    pub fn new(
+        configuration: Configuration,
+        transport: Box<dyn Transport>,
+        monitor: Box<dyn ClientMonitor>,
+    ) -> Self {
         let max_connections = configuration.max_connections;
 
         Self {
@@ -64,18 +68,26 @@ impl Client {
         }
     }
 
-    pub fn connect(&mut self, remote_address: SocketAddr, secret: Secret, connection_token: ConnectionToken) -> Result<Connection, Error> {
-
+    pub fn connect(
+        &mut self,
+        remote_address: SocketAddr,
+        secret: Secret,
+        connection_token: ConnectionToken,
+    ) -> Result<Connection, Error> {
         if self.address_to_connection.contains_key(&remote_address) {
-            return Err(ClientError::AlreadyConnectedToAddress{ address: remote_address }.into());
+            return Err(ClientError::AlreadyConnectedToAddress {
+                address: remote_address,
+            }
+            .into());
         }
 
         if let Some(connection) = self.connections.create_connection() {
-
             self.addresses.set(connection, remote_address);
-            self.address_to_connection.insert(remote_address, connection);
+            self.address_to_connection
+                .insert(remote_address, connection);
             self.timeouts.set(connection, self.configuration.timeout);
-            self.heartbeats.set(connection, self.configuration.heartbeat);
+            self.heartbeats
+                .set(connection, self.configuration.heartbeat);
             self.sequence_numbers.set(connection, 0);
             self.states.set(connection, ConnectionState::Connecting);
             self.secrets.set(connection, secret);
@@ -88,7 +100,6 @@ impl Client {
             self.monitor.connecting();
 
             Ok(connection)
-
         } else {
             Err(ClientError::MaximumConnectionsReached.into())
         }
@@ -99,7 +110,7 @@ impl Client {
         let mut events = Vec::new();
 
         self.monitor.tick();
-        
+
         while poll_again {
             let mut buffer = [0; 1500];
             match self.transport.poll(&mut buffer) {
@@ -111,54 +122,48 @@ impl Client {
                     } else {
                         println!("message from unknown source");
                     }
-                },
+                }
                 Ok(None) => {
                     poll_again = false;
-                },
+                }
                 Err(error) => {
                     println!("{}", error);
-                },
+                }
             }
         }
 
         let connections: Vec<Connection> = self.connections.into_iter().collect();
         for connection in connections {
-            
             // manage timeouts
-            let timeout = self.timeouts.get(connection).expect("No timeout set for connection") - 1;
+            let timeout = self
+                .timeouts
+                .get(connection)
+                .expect("No timeout set for connection")
+                - 1;
             if timeout == 0 {
-                let address = self.addresses.get(connection).unwrap();
-                
-                self.address_to_connection.remove(address);
-                self.states.set(connection, ConnectionState::Disconnected);
-                self.addresses.remove(connection);
-                self.timeouts.remove(connection);
-                self.heartbeats.remove(connection);
-                self.sequence_numbers.remove(connection);
-                self.secrets.remove(connection);
-                self.connection_tokens.remove(connection);
-
-                self.connections.delete_connection(connection).unwrap();
-                self.monitor.disconnected();
-                events.push(Event::Disconnected { connection });
+                self.disconnect(connection, &mut events);
                 continue;
-
             } else {
                 self.timeouts.set(connection, timeout);
             }
 
             // manage heartbeats
-            let heartbeat = self.heartbeats.get(connection).expect("No heartbeat set for connection") - 1;
+            let heartbeat = self
+                .heartbeats
+                .get(connection)
+                .expect("No heartbeat set for connection")
+                - 1;
             if heartbeat == 0 {
-
                 let state = self.get_connection_state(connection);
                 match state {
                     Some(ConnectionState::Connected) => {
-                        self.send_heartbeat_message(connection).expect("Could not send heartbeat message");
-                    },
+                        self.send_heartbeat_message(connection)
+                            .expect("Could not send heartbeat message");
+                    }
                     Some(ConnectionState::Connecting) => {
-                        self.send_connection_message(connection).expect("Could not send connection message");
-                    },
+                        self.send_connection_message(connection)
+                            .expect("Could not send connection message");
+                    }
                     _ => {
                         panic!("this should not happen");
                     }
@@ -176,33 +181,70 @@ impl Client {
         match self.get_connection_state(connection) {
             Some(ConnectionState::Connected) => {
                 Ok(self.send_internal(packet, connection, PacketType::Payload)?)
-            },
-            Some(ConnectionState::Connecting) => {
-                Err(ClientError::ConnectionStillConnecting.into())
-            },
-            Some(ConnectionState::Disconnected) => {
-                Err(ClientError::ConnectionDisconnected.into())
-            },
-            None => {
-                Err(ClientError::ConnectionNotFound.into())
-            },
+            }
+            Some(ConnectionState::Connecting) => Err(ClientError::ConnectionStillConnecting.into()),
+            Some(ConnectionState::Disconnected) => Err(ClientError::ConnectionDisconnected.into()),
+            None => Err(ClientError::ConnectionNotFound.into()),
         }
     }
 
-    fn send_internal(&mut self, packet: OutgoingPacket, connection: Connection, packet_type: PacketType) -> Result<u64, Error> {
-        let sequence_number = self.sequence_numbers.get(connection).expect("No sequence number for connection found") + 1;
+    pub fn disconnect(&mut self, connection: Connection, events: &mut Vec<Event>) {
+        let address = self.addresses.get(connection).unwrap();
+
+        self.address_to_connection.remove(address);
+        self.states.set(connection, ConnectionState::Disconnected);
+        self.addresses.remove(connection);
+        self.timeouts.remove(connection);
+        self.heartbeats.remove(connection);
+        self.sequence_numbers.remove(connection);
+        self.secrets.remove(connection);
+        self.connection_tokens.remove(connection);
+
+        self.connections.delete_connection(connection).unwrap();
+        self.monitor.disconnected();
+        events.push(Event::Disconnected { connection });
+    }
+
+    fn send_internal(
+        &mut self,
+        packet: OutgoingPacket,
+        connection: Connection,
+        packet_type: PacketType,
+    ) -> Result<u64, Error> {
+        let sequence_number = self
+            .sequence_numbers
+            .get(connection)
+            .expect("No sequence number for connection found")
+            + 1;
         self.sequence_numbers.set(connection, sequence_number);
-        let secret = self.secrets.get(connection).expect("No secret for connection found");
+        let secret = self
+            .secrets
+            .get(connection)
+            .expect("No secret for connection found");
 
-        let (ack_sequence_number, ack_bits) = self.replay_buffers.get(connection).expect("no replay buffer for connection").get_ack_bits();
+        let (ack_sequence_number, ack_bits) = self
+            .replay_buffers
+            .get(connection)
+            .expect("no replay buffer for connection")
+            .get_ack_bits();
 
-        let raw = packet.write_header_and_sign(sequence_number, ack_sequence_number, ack_bits, packet_type.to_u8(), secret);
-        let address = self.addresses.get(connection).expect("No address for connection found");
+        let raw = packet.write_header_and_sign(
+            sequence_number,
+            ack_sequence_number,
+            ack_bits,
+            packet_type.to_u8(),
+            secret,
+        );
+        let address = self
+            .addresses
+            .get(connection)
+            .expect("No address for connection found");
 
         // TODO check bytes sent?
         let _bytes_sent = self.transport.send(address, raw.get_buffer())?;
 
-        self.heartbeats.set(connection, self.configuration.heartbeat);
+        self.heartbeats
+            .set(connection, self.configuration.heartbeat);
         self.monitor.message_sent();
 
         Ok(sequence_number)
@@ -222,20 +264,38 @@ impl Client {
         }
     }
 
-    fn handle_message(&mut self, connection: Connection, packet: RawPacket, events: &mut Vec<Event>) {
-        let secret = self.secrets.get(connection).expect("Secret for connection not found");
+    fn handle_message(
+        &mut self,
+        connection: Connection,
+        packet: RawPacket,
+        events: &mut Vec<Event>,
+    ) {
+        let secret = self
+            .secrets
+            .get(connection)
+            .expect("Secret for connection not found");
 
         if let Some(incoming) = packet.verify(secret) {
-            let state = self.states.get(connection).expect("State for connection not found").clone();
+            let state = self
+                .states
+                .get(connection)
+                .expect("State for connection not found")
+                .clone();
 
             let sequence_number = incoming.get_sequence_number();
-            let replay_buffer = self.replay_buffers.get_mut(connection).expect("No replay buffer for connection");
+            let replay_buffer = self
+                .replay_buffers
+                .get_mut(connection)
+                .expect("No replay buffer for connection");
 
             if replay_buffer.acknowledge(sequence_number) {
                 let ack_sequence_number = incoming.get_ack_sequence_number();
                 let ack_bits = incoming.get_ack_bits();
 
-                let ack_buffer = self.ack_buffers.get_mut(connection).expect("no replay buffer for connection");
+                let ack_buffer = self
+                    .ack_buffers
+                    .get_mut(connection)
+                    .expect("no replay buffer for connection");
                 let acked = ack_buffer.set_ack_bits(ack_sequence_number, ack_bits);
 
                 for sequence_number in acked {
@@ -249,7 +309,8 @@ impl Client {
                 if state == ConnectionState::Connecting {
                     self.states.set(connection, ConnectionState::Connected);
                     self.timeouts.set(connection, self.configuration.timeout);
-                    self.heartbeats.set(connection, self.configuration.heartbeat);
+                    self.heartbeats
+                        .set(connection, self.configuration.heartbeat);
                     self.connection_tokens.remove(connection);
 
                     self.monitor.connected();
@@ -263,7 +324,6 @@ impl Client {
                         });
                     }
                 } else {
-
                     match incoming.get_packet_type() {
                         Some(PacketType::Payload) => {
                             self.timeouts.set(connection, self.configuration.timeout);
@@ -273,10 +333,10 @@ impl Client {
                                 connection,
                                 payload: incoming.into_payload(),
                             });
-                        },
+                        }
                         Some(PacketType::Heartbeat) => {
                             self.timeouts.set(connection, self.configuration.timeout);
-                        },
+                        }
                         Some(packet_type) => {
                             println!("got unexpected packet type {:?}", packet_type);
                         }
@@ -295,12 +355,15 @@ impl Client {
 
     fn send_connection_message(&mut self, connection: Connection) -> Result<(), Error> {
         use std::io::Write;
-        
-        let connection_token = self.connection_tokens.get(connection).expect("No connection token for connection");
+
+        let connection_token = self
+            .connection_tokens
+            .get(connection)
+            .expect("No connection token for connection");
 
         let mut packet = OutgoingPacket::new();
         packet.write(connection_token.get_bytes())?;
-        
+
         self.send_internal(packet, connection, PacketType::Connection)?;
 
         Ok(())
